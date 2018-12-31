5baf51299e410fabda7f445319287ec6544ef34c
2201297119 <2201297119@qq.com>
2018-12-31 16:11:33 +0800

    Test

--- Hello.java
+++ Hello.java
@@ -3,8 +3,7 @@ public class Hello{
 	
 	public static void main(String args[]){
 		
-		System.out.println("你好世界!");
-		System.out.println("Hello Git")
+		System.out.println("Hello William");
 	}
 	
 }
